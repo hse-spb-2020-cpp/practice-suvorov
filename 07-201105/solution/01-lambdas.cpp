367aaf62ba0798b42c253123fee27285551b4e60
Egor Suvorov <egor.suvorov@gmail.com>
2020-11-05 10:54:01 +0300

    07-201105: add solution/

---
+++ 07-201105/solution/01-lambdas.cpp
@@ -0,0 +1,61 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <algorithm>
+#include <vector>
+
+int count_even(const std::vector<int> &vec) {
+    return std::count_if(vec.begin(), vec.end(), [](int x) { return x % 2 == 0; });
+}
+
+TEST_CASE("count_even") {
+    CHECK(count_even(std::vector<int>{}) == 0);
+    CHECK(count_even(std::vector{1, 2, 3, 4, 5}) == 2);
+    CHECK(count_even(std::vector{1, 2, 3, 4, 2, 6}) == 4);
+}
+
+int count_divisible_by(const std::vector<int> &vec, int modulo) {
+    return std::count_if(vec.begin(), vec.end(), [&](int x) { return x % modulo == 0; });
+}
+
+TEST_CASE("count_divisible_by") {
+    CHECK(count_divisible_by(std::vector<int>{}, 3) == 0);
+    CHECK(count_divisible_by(std::vector{1, 2, 3, 4, 5}, 3) == 1);
+    CHECK(count_divisible_by(std::vector{1, 2, 3, 4, 2, 6}, 3) == 2);
+    CHECK(count_divisible_by(std::vector{1, 2, 3, 4, 2, 6}, 4) == 1);
+    CHECK(count_divisible_by(std::vector{1, 2, 3, 4, 2, 6}, 2) == 4);
+}
+
+TEST_CASE("sort vectors by length") {
+    std::vector<std::vector<int>> data{
+        {1, 2, 3},
+        {4, 5},
+        {6, 7, 8, 9},
+        {6, 7, 0, 0},
+        {6, 7, 10, 10},
+        {1, 1, 1, 1, 1}
+    };
+    sort(data.begin(), data.end(), [](const std::vector<int> &a, const std::vector<int> &b) {
+        if (a.size() != b.size()) return a.size() < b.size();
+        return a < b;
+    });
+
+    CHECK((data == std::vector<std::vector<int>>{
+        {4, 5},
+        {1, 2, 3},
+        {6, 7, 0, 0},
+        {6, 7, 8, 9},
+        {6, 7, 10, 10},
+        {1, 1, 1, 1, 1}
+    }));
+}
+
+TEST_CASE("generator") {
+    auto generator = [sequence_id = 0]() mutable {
+        return sequence_id++;
+    };
+
+    CHECK(generator() == 0);
+    CHECK(generator() == 1);
+    CHECK(generator() == 2);
+    CHECK(generator() == 3);
+}
---
+++ 07-201105/solution/03-min-holder.cpp
@@ -0,0 +1,91 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <cassert>
+#include <cmath>
+#include <optional>
+
+template<typename T, typename Compare = std::less<T>>
+struct MinHolder {
+private:
+    std::optional<T> min_value_;
+    Compare compare_;
+
+public:
+    MinHolder(const Compare &compare = Compare()) : compare_(compare) {}
+
+    void operator()(const T &value) {
+        if (!min_value_ || compare_(value, *min_value_)) {
+            min_value_.emplace(value);
+        }
+    }
+
+    const T &get() const {
+        assert(min_value_);
+        return *min_value_;
+    }
+};
+
+TEST_CASE("default constructor is enough") {
+    MinHolder<int> h;
+    h(0);
+    CHECK(h.get() == 0);
+    h(-10);
+    h(-20);
+    h(-15);
+    CHECK(h.get() == -20);
+    h(-20);
+    CHECK(h.get() == -20);
+    h(-21);
+    CHECK(h.get() == -21);
+}
+
+TEST_CASE("custom comparator with default ctor") {
+    MinHolder<int, std::greater<int>> h;
+    h(10);
+    h(20);
+    h(15);
+    CHECK(h.get() == 20);
+    h(20);
+    CHECK(h.get() == 20);
+    h(21);
+    CHECK(h.get() == 21);
+}
+
+TEST_CASE("custom comparator with default ctor") {
+    auto cmp = [](int a, int b) { return std::abs(a - 10) < std::abs(b - 10); };
+    MinHolder<int, decltype(cmp)> h(cmp);
+    h(8);
+    h(4);
+    CHECK(h.get() == 8);
+    h(11);
+    CHECK(h.get() == 11);
+    h(10);
+    CHECK(h.get() == 10);
+    h(9);
+    CHECK(h.get() == 10);
+}
+
+TEST_CASE("default constructor is not enough") {
+    MinHolder<int> h;
+    h(10);
+    h(20);
+    h(15);
+    CHECK(h.get() == 10);
+    h(8);
+    CHECK(h.get() == 8);
+    h(-100);
+    CHECK(h.get() == -100);
+}
+
+TEST_CASE("no default constructor") {
+    struct Foo {
+        explicit Foo(int, int, int) {}
+    };
+    auto all_equal = [](const auto&, const auto&) { return false; };
+
+    // Hint: use std::optional<> inside MinHolder<>.
+    MinHolder<Foo, decltype(all_equal)> h(all_equal);
+    h(Foo(1, 2, 3));
+    h(Foo(4, 5, 6));
+    [[maybe_unused]] Foo f = h.get();
+}
